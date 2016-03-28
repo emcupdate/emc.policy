c3aec6634983140622ba0bf8da5d210b0e960d7d
adam139 <yuejun.tang@gmail.com>
2016-03-28 09:51:10 +0800

    for file provide external edit view

---
+++ emc/policy/browser/file.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from plone.app.contenttypes.browser.utils import Utils
+from AccessControl import getSecurityManager
+# from AccessControl import Unauthorized
+from Products.CMFCore.permissions import ModifyPortalContent
+
+class FileView(Utils):
+
+    def __init__(self, context, request):
+        super(FileView, self).__init__(context, request)
+
+    def is_videotype(self):
+        ct = self.context.file.contentType
+        return 'video/' in ct
+
+    def is_audiotype(self):
+        ct = self.context.file.contentType
+        return 'audio/' in ct
+
+    def get_mimetype_icon(self):
+        return super(FileView, self).getMimeTypeIcon(self.context.file)
+    
+    def isEditable(self):
+        sm = getSecurityManager()
+        return sm.checkPermission(ModifyPortalContent, self.context)
+            
