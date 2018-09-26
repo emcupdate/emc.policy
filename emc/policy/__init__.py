f0dcd5a80f5b4bafd6988a07587dfbba26ffdc79
adam139 <yuejun.tang@gmail.com>
2018-09-26 21:43:02 +0800

    patch PlonePAS replacing with local Products.PlonePAS development version,and add utility functions

--- emc/policy/__init__.py
+++ emc/policy/__init__.py
@@ -1,4 +1,4 @@
-
+#-*- coding: UTF-8 -*-
 from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('emc.policy')
@@ -6,87 +6,40 @@ _ = MessageFactory('emc.policy')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
-# pas alterations and monkies  
-# here patch for Products.PlonePAS.pas
-from AccessControl import Unauthorized
-from AccessControl import getSecurityManager
-from AccessControl.PermissionRole import PermissionRole
-from AccessControl.Permissions import change_permissions
-from AccessControl.Permissions import manage_properties
-from AccessControl.Permissions import manage_users as ManageUsers
-from AccessControl.requestmethod import postonly
-from OFS.Folder import Folder
-from Products.CMFCore.utils import getToolByName
-from Products.CMFCore.utils import registerToolInterface
-from Products.PlonePAS.interfaces.group import IGroupIntrospection
-from Products.PlonePAS.interfaces.group import IGroupManagement
-from Products.PlonePAS.interfaces.plugins import ILocalRolesPlugin
-from Products.PlonePAS.interfaces.plugins import IUserIntrospection
-from Products.PlonePAS.interfaces.plugins import IUserManagement
-from Products.PlonePAS.patch import ORIG_NAME
-from Products.PlonePAS.patch import wrap_method
-from Products.PluggableAuthService.PluggableAuthService import \
-    PluggableAuthService
-from Products.PluggableAuthService.PluggableAuthService import \
-    _SWALLOWABLE_PLUGIN_EXCEPTIONS
-from Products.PluggableAuthService.events import PrincipalDeleted
-from emc.policy.events import DeleteMemberEvent
-from Products.PluggableAuthService.interfaces.authservice import \
-    IPluggableAuthService
-from Products.PluggableAuthService.interfaces.plugins import \
-    IAuthenticationPlugin
-from Products.PluggableAuthService.interfaces.plugins import \
-    IGroupEnumerationPlugin
-from Products.PluggableAuthService.interfaces.plugins import \
-    IRoleAssignerPlugin
-from Products.PluggableAuthService.interfaces.plugins import \
-    IUserEnumerationPlugin
-from zope.event import notify
-import logging
 
 
-logger = logging.getLogger('PlonePAS')
+import datetime
+fmt = "%Y-%m-%d %H:%M:%S"
 
-registerToolInterface('acl_users', IPluggableAuthService)
-
-
-def _doDelUser(self, id):
-    """
-    Given a user id, hand off to a deleter plugin if available.
-    """
-    plugins = self._getOb('plugins')
-    import pdb
-    pdb.set_trace()
-#     context = aq_inner(self)
-    mtool = getToolByName(self, 'portal_membership')
-    current = mtool.getAuthenticatedMember()
-  
-    userdeleters = plugins.listPlugins(IUserManagement)
-
-    if not userdeleters:
-        raise NotImplementedError(
-            "There is no plugin that can delete users."
-        )
-
-    for userdeleter_id, userdeleter in userdeleters:
-        try:
-            user = mtool.getMemberById(id)
-            userdeleter.doDeleteUser(id)
-        except _SWALLOWABLE_PLUGIN_EXCEPTIONS:
-            pass
+def list2str(lst):
+    "transfer a list to string"
+    initstr = ""
+    for i in lst:
+        if len(initstr) == 0 :initstr ="%s" % str(i)        
         else:
-            notify(PrincipalDeleted(id))
-            notify(DeleteMemberEvent(adminid = current.getId(),
-                                     userid = user.getId(),
-                                     datetime = '2018-09-09 13:21:22',
-                                     ip = '192.168.2.1',
-                                     type = 0,
-                                     text = "delete user",
-                                     result = 1))
+            initstr = "%s,%s" %(initstr,str(i))
+    return initstr
+        
+    
+def get_ip(request = None):
+    """ Extract the client IP address from the HTTP request in a proxy-compatible way.
 
-wrap_method(
-        PluggableAuthService,
-        '_doDelUser',
-        _doDelUser,
-        add=True
-    )
+    @return: IP address as a string or None if not available
+    """
+    if request == None:
+        from zope.globalrequest import getRequest
+        request = getRequest()
+    if request == None:return ""    
+    if "HTTP_X_FORWARDED_FOR" in request.environ:
+        # Virtual host
+        ip = request.environ["HTTP_X_FORWARDED_FOR"]
+    elif "HTTP_HOST" in request.environ:
+        # Non-virtualhost
+        ip = request.environ["REMOTE_ADDR"]
+    else:
+        # Unit test code?
+        ip = ""
+
+    return ip
+
+       
