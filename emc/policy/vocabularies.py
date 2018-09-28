4e9a4656492b9da48f597d572f860f9347b99552
adam139 <yuejun.tang@gmail.com>
2018-09-28 10:04:51 +0800

    add i18n function

--- emc/policy/vocabularies.py
+++ emc/policy/vocabularies.py
@@ -3,11 +3,11 @@ from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.interfaces import IVocabularyFactory
 from zope.interface import provider
+from Products.CMFPlone import PloneMessageFactory as _
 
-
-items = [ ('low', u'secret'),
-          ('mid', u'more secret'),
-          ('height', u'most secret')          
+items = [ ('low', _('secret')),
+          ('mid', _('more secret')),
+          ('height', _('most secret'))          
           ]
 terms = [ SimpleTerm(value=pair[0], token=pair[0], title=pair[1]) for pair in items ]
 Vocabulary = SimpleVocabulary(terms)
