de9fc223e9d1429286a16a41cdf2ece1db13162d
adam139 <yuejun.tang@gmail.com>
2018-12-02 10:57:01 +0800

    update vocabulary

--- emc/policy/vocabularies.py
+++ emc/policy/vocabularies.py
@@ -4,10 +4,23 @@ from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.interfaces import IVocabularyFactory
 from zope.interface import provider
 from Products.CMFPlone import PloneMessageFactory as _
+"""
+非密  60
+内部  65
+一般  70
+重要  80
+核心  90
+"""
 
-items = [ ('low', _('secret')),
-          ('mid', _('more secret')),
-          ('height', _('most secret'))          
+# items = [ ('low', _('secret')),
+#           ('mid', _('more secret')),
+#           ('height', _('most secret'))          
+#           ]
+items = [ ('60', _('no secret')),
+          ('65', _('inner')),
+          ('70', _('normal secret')), 
+          ('80', _('more secret')),
+          ('90', _('most secret')),                     
           ]
 terms = [ SimpleTerm(value=pair[0], token=pair[0], title=pair[1]) for pair in items ]
 Vocabulary = SimpleVocabulary(terms)
